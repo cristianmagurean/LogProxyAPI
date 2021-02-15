1fe84b3f2c919292bbabb150d2d4f98b6d3f1456
Cristian Magurean <cristian_magurean@yahoo.com>
2021-02-15 16:45:57 +0200

    fixes

--- LogProxyAPI/Controllers/LogProxy/DTO/SaveRequestDto.cs
+++ LogProxyAPI/Controllers/LogProxy/DTO/SaveRequestDto.cs
@@ -1,8 +1,12 @@
-﻿namespace LogProxyAPI.Controllers.LogProxy.DTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LogProxyAPI.Controllers.LogProxy.DTO
 {
     public class SaveRequestDto
     {
+        [Required]
         public string Text { get; set; }
+        [Required]
         public string Title { get; set; }
     }
 }
--- LogProxyAPI/Controllers/LogProxy/LogProxyController.cs
+++ LogProxyAPI/Controllers/LogProxy/LogProxyController.cs
@@ -3,6 +3,7 @@ using LogProxyAPI.CQRS;
 using LogProxyAPI.Entities;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Net;
@@ -24,6 +25,7 @@ namespace LogProxyAPI.Controllers
 
         [HttpGet("messages")]
         [ProducesResponseType(typeof(IEnumerable<Message>), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<ActionResult<IEnumerable<Message>>> GetMessages()
         {           
             return Ok(await _mediator.Send(new GetMessagesQuery()));
@@ -31,6 +33,7 @@ namespace LogProxyAPI.Controllers
 
         [HttpPost("messages")]
         [ProducesResponseType(typeof(SaveResponseDTO), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<ActionResult<SaveResponseDTO>> SaveMessage([FromBody] SaveRequestDto request)
         {            
             return Ok(await _mediator.Send(new SaveMessageCommand(request.Title, request.Text)));            
